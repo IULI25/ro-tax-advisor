08b6804adc0ef8043848a192256c7c5531cfddaf
IULI25 <iulian25florea@gmail.com>
2026-07-30 19:26:59 +0300

    clean

--- streamlit_chatbox/thirdpart.py
+++
@@ -1,7 +0,0 @@
-from streamlit_markdown import st_markdown, st_hack_markdown
-
-from .messages import ChatBox
-
-
-# ChatBox.register_output_method("richmd", st_markdown)
-ChatBox.register_output_method("richmd", st_hack_markdown)
