0243fa6e215c3de2f75d97a999f4f0560cc10a94
IULI25 <iulian25florea@gmail.com>
2026-09-08 20:22:06 +0300

    1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from embedding import indexeaza_fisier_html
 st.set_page_config(page_title="Agent AI din pagină web", page_icon="🤖", layout="centered")
 st.title("🤖 Consilier AI")
 
-FISIER_HTML_LOCAL = "Legea_nr.227_2015.html"
+FISIER_HTML_LOCAL = "Legea_nr_227_2015.html"
 MODELE_DISPONIBILE = ["gemini-3.7-flash","gemini-3.6-flash", "gemini-3.5-flash", "gemini-3.5-flash-lite", "gemini-3.1-flash-lit"]
 
 api_key = st.secrets.get("GEMINI_API_KEY")
