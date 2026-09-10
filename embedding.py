fdacf8805dd867e639193c86bb3dc8ec3252ddd9
IULI25 <iulian25florea@gmail.com>
2026-09-10 18:34:13 +0300

    1

--- embedding.py
+++ embedding.py
@@ -10,7 +10,7 @@ from sentence_transformers import SentenceTransformer
 
 from chunking import extrage_text_din_html, genereaza_chunkuri_finale
 
-MODEL_NAME = "intfloat/multilingual-e5-base"
+MODEL_NAME = "sentence-transformers/BAAI-bge-m3"
 DEFAULT_PERSIST_DIR = "./chroma_db"
 DEFAULT_COLLECTION = "documente_legislative"
 
